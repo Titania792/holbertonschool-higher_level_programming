682579dfb60750fee017060fc2aa8c74cd462d90
Titania792 <darkwarrior792@gmail.com>
2022-04-25 21:09:41 -0700

    trying

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+ldig = (number % 10)
+if ldig > 5:
+	ifel = "and is greater than 5"
+elif ldig == 0:
+	ifel = "and is 0"
+else:
+	ifel = "and is less than 6 and not 0"
+print(f"Last digit of {number} is {ldig} {ifel}")
