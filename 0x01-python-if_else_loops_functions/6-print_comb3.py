124ec21e47e69ce77cac14a69cbdebd13bddcb03
Titania792 <darkwarrior792@gmail.com>
2022-04-27 06:24:56 -0700

    6-print_comb3.py fixed

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,7 @@ numbers = 0
 for fdig in range(9):
     numbers = numbers + 1
     for sdig in range(numbers, 10):
+        if fdig == 8:
+            break
         print(f"{fdig}{sdig}, ", end="")
 print(f"89")
