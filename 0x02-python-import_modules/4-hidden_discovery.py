e40ac4a6065027394c28eac1f5abc94937b17194
Titania792 <darkwarrior792@gmail.com>
2022-04-28 09:12:12 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for i in range(len(dir(hidden_4)))
-        if i == '_'
-
-    print(dir(hidden_4))
+    lis = dir(hidden_4)
+    for i in lis:
+        if not i.startswith('__', 0):
+            print(i)
