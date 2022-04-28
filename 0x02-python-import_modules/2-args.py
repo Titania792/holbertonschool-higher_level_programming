c2af339e93ebdd42d614586ea57a099ce0cd93d6
Titania792 <darkwarrior792@gmail.com>
2022-04-28 08:10:21 -0700

    checker2.0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-import sys
-num = len(sys.argv) - 1
-if num == 0:
-    div = '.'
-else:
-    div = ':'
-if num == 1:
-    arg = "argument"
-else:
-    arg = "arguments"
+if __name__ == "__main__":
+    import sys
+    num = len(sys.argv) - 1
+    if num == 0:
+        div = '.'
+    else:
+        div = ':'
+    if num == 1:
+        arg = "argument"
+    else:
+        arg = "arguments"
 
-print(f"{num} {arg}{div}")
-for i in range(1, (num + 1)):
-    print(f"{i}: {str(sys.argv[i])}")
+    print(f"{num} {arg}{div}")
+    for i in range(1, (num + 1)):
+        print(f"{i}: {str(sys.argv[i])}")
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import sys
-leng = len(sys.argv)
-res = 0
-for i in range(1, leng):
-    res = res + int(sys.argv[i])
-print(f"{res}")
+if __name__ == "__main__":
+    import sys
+    leng = len(sys.argv)
+    res = 0
+    for i in range(1, leng):
+        res = res + int(sys.argv[i])
+    print(f"{res}")
