b559edfd67ccc3b40e78490d907480ce1187ca6d
Titania792 <darkwarrior792@gmail.com>
2022-04-28 08:04:48 -0700

    checker

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import sys
+leng = len(sys.argv)
+res = 0
+for i in range(1, leng):
+    res = res + int(sys.argv[i])
+print(f"{res}")
---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import hidden_4
+for i in range(len(dir(hidden_4)))
+    if i == '_'
+
+print(dir(hidden_4))
