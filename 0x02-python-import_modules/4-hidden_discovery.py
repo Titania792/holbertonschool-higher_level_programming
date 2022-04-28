0f9fb813568d1be4f8f2026adf1ffb3e36edc691
Titania792 <darkwarrior792@gmail.com>
2022-04-28 08:16:45 -0700

    5-variable_load.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
-for i in range(len(dir(hidden_4)))
-    if i == '_'
+if __name__ == "__main__":
+    import hidden_4
+    for i in range(len(dir(hidden_4)))
+        if i == '_'
 
-print(dir(hidden_4))
+    print(dir(hidden_4))
