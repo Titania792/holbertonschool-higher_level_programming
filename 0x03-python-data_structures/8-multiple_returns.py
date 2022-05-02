ca9ed76e59134b0807439f6b64a702e228f3a961
Titania792 <darkwarrior792@gmail.com>
2022-05-02 14:52:37 -0700

    8 :D

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence:
-        lsen = list(sentence)
-        tuple_a = (len(lsen), lsen[0])
-        if len(lsen) == 0:
-            lsen[0] = None
-        return tuple_a
+        return (len(sentence), sentence[0])
+    else:
+        return (0, None)
