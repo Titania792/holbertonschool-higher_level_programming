8b31f4eb9109f54fe4de3c558cd651ee8f4b9b57
Titania792 <darkwarrior792@gmail.com>
2022-05-02 15:33:46 -0700

    10 bby

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    a = []
-    for i in my_list:
-        if my_list[i] % 2 == 0:
-            a.insert(i, True)
-        else:
-            a.insert(i, False)
-    return a
+    if my_list:
+        a = []
+        for i in my_list:
+            if my_list[i] % 2 == 0:
+                a.insert(i, True)
+            else:
+                a.insert(i, False)
+        return a
