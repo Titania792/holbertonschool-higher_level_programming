d8e3cf2850669fa26bea07459751e5fed49ba94c
Titania792 <darkwarrior792@gmail.com>
2022-05-16 10:07:33 -0700

    task 0 pep8

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    l = 0
+    large = 0
     while l < x:
         try:
-             print("{}".format(my_list[l]), end = "")
+            print("{}".format(my_list[large]), end="")
         except IndexError:
             break
-        l += 1
+        large += 1
     print("")
-    return (l)
+    return (large)
