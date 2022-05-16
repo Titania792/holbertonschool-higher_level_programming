a2d67b37e7a2aaf7709502cd420e77e40d20fd04
Titania792 <darkwarrior792@gmail.com>
2022-05-16 11:37:02 -0700

    task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    large = 0
+    nprint = 0
+    while large < x:
+        try:
+            print("{:d}".format(my_list[large]), end="")
+            nprint += 1
+        except (TypeError, ValueError):
+            large += 1
+        large += 1
+    print("")
+    return (nprint)
