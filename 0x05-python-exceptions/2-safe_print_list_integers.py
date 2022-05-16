564eb2001133b5ca78f4645a957b4c6b754784f4
Titania792 <darkwarrior792@gmail.com>
2022-05-16 13:13:01 -0700

    task 2 fix

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,7 @@
 def safe_print_list_integers(my_list=[], x=0):
     large = 0
     nprint = 0
-    while large <= x:
+    while large < x:
         try:
             print("{:d}".format(my_list[large]), end="")
             nprint += 1
