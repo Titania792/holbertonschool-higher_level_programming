ef4a75b76929c76fe7806b546488c2e8892ee5f9
Titania792 <darkwarrior792@gmail.com>
2022-05-16 11:41:44 -0700

    commit

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[large]), end="")
             nprint += 1
         except (TypeError, ValueError):
-            large += 1
+            break
         large += 1
     print("")
     return (nprint)
