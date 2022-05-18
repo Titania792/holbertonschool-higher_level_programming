3967db8d4057601590742ae90e35de9a9f9572ed
Titania792 <darkwarrior792@gmail.com>
2022-05-18 14:45:43 -0700

    fixing p.1

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -40,4 +40,4 @@ class Rectangle:
         return (self.__height * self.__width)
 
     def perimeter(self):
-        return ((self.__height * 2)+ (self.__width * 2))
+        return ((self.__height * 2) + (self.__width * 2))
