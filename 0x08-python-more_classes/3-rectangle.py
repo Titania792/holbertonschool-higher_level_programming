ffbdb917f9c0688e98b8ed09bf175711048709d5
Titania792 <darkwarrior792@gmail.com>
2022-05-18 12:00:30 -0700

    task 2,3 and 4 check try

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""Function that writes class Rectangle and defines it by:
+Private instance attribute
+Instantiation with optional
+Public instance method"""
+
+
+class Rectangle:
+    """Creating/defining Rectangle class"""
+    def __init__(self, width=0, height=0):
+        """Initializes the class with the proper characteristics"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.__height * self.__width)
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return ((self.__height * 2) + (self.__width * 2))
+
+    def __str__(self):
+        out = ""
+        if self.__height == 0 or self.__width == 0:
+            out += "\n"
+        else:
+            for j in range(self.__height):
+                for i in range(self.__width):
+                    out += "#"
+                out += "\n"
+        return out
+
+    def my_print(self):
+        print(str(self))
+        return self.my_print()
