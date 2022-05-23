66244a280f597bc2e8a5a9d91094fefa38e8c431
Tatiana Fernandez <97978908+Titania792@users.noreply.github.com>
2022-05-23 11:32:48 -0300

    fixing  task 0 and doing task 2

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""aaa"""
+"""function that adds 2 integers"""
 
 
 def add_integer(a, b=98):
-    """bbb"""
+    """this function returns the add of two numbers"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
