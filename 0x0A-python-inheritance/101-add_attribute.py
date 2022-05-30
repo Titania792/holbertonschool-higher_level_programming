33e7a61e2141cb1d9ee29c171347e710a79d7c23
Tatiana Fernandez <97978908+Titania792@users.noreply.github.com>
2022-05-30 18:12:55 -0300

    last fixing

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""function that adds a new attribute"""
+
+
+def add_attribute(a_class, a_name, a_value):
+    """adding attribute if it's possible"""
+    if hasattr(a_class, '__dict__'):
+        setattr(a_class, a_name, a_value)
+    else:
+        raise TypeError("can't add new attribute")
