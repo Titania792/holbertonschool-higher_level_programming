f5ae0e2227cb70ed2431c136644102f7b265f7ba
Titania792 <darkwarrior792@gmail.com>
2022-05-31 18:44:04 -0300

    task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,9 +3,6 @@
 simple data structure for JSON serialization of an object"""
 
 
-import json
-
-
 def class_to_json(obj):
     """returns the dictionary description with simple data structure
     for JSON serialization of an object"""
