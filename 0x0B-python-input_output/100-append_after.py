2c8e2cc7310f7380cc9405794c4da03c0dbd52af
Tatiana Fernandez <97978908+Titania792@users.noreply.github.com>
2022-05-31 12:32:55 -0300

    readme and w8ing checker

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 """Write a function that inserts a line of text to a file,
 after each line containing a specific string"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Write a function that inserts a line of text to a file,"""
+    with open(filename, "r+") as f:
+        lines = f.readlines()
+        f.seek(0)
+        for line in lines:
+            if search_string in line:
+                f.write(new_string)
+            f.write(line)
+        f.truncate()
