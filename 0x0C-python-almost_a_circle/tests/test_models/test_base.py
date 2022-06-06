824d8e1381ca4e8a5a8510fa8199424673a09835
Tatiana Fernandez <97978908+Titania792@users.noreply.github.com>
2022-06-06 17:20:25 -0300

    tests 1

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,6 +2,8 @@
 """Unittest for Base Class"""
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
@@ -9,16 +11,48 @@ class TestBase(unittest.TestCase):
 
     def test_base_class(self):
         """Test for Base Class"""
-        b1 = Base()
-        b2 = Base()
+        b1 = Base(-1)
+        b2 = Base(0)
         b3 = Base()
         b4 = Base(12)
         b5 = Base()
-        self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 2)
-        self.assertEqual(b3.id, 3)
+        self.assertEqual(b1.id, -1)
+        self.assertEqual(b2.id, 0)
+        self.assertEqual(b3.id, 1)
         self.assertEqual(b4.id, 12)
-        self.assertEqual(b5.id, 4)
+        self.assertEqual(b5.id, 2)
+
+    def test_to_json_string(self):
+        """Test for to_json_string"""
+        list_dictionaries = [{'id': 1, 'x': 2, 'y': 3},
+                             {'id': 2, 'x': 4, 'y': 5}]
+        self.assertEqual(Base.to_json_string(list_dictionaries),
+                         '[{"id": 1, "x": 2, "y": 3}, {"id": 2, "x": 4, "y": 5}]')
+        self.assertEqual(Base.to_json_string(None), "[]")
+        self.assertEqual(Base.to_json_string([]), "[]")
+        self.assertEqual(Base.to_json_string([{'id': 1, 'x': 2, 'y': 3}]),
+                         '[{"id": 1, "x": 2, "y": 3}]')
+        self.assertEqual(Base.to_json_string([{'id': 1, 'x': 2, 'y': 3},
+                                              {'id': 2, 'x': 4, 'y': 5}]),
+                         '[{"id": 1, "x": 2, "y": 3}, {"id": 2, "x": 4, "y": 5}]')
+        self.assertTrue(Base.to_json_string(list_dictionaries)
+                        is not Base.to_json_string(None))
+        self.assertTrue(Base.to_json_string(list_dictionaries)
+                        is not Base.to_json_string([]))
+        self.assertTrue(Base.to_json_string(list_dictionaries)
+                        is not Base.to_json_string([{'id': 1, 'x': 2, 'y': 3}]))
+        self.assertTrue(Base.to_json_string(list_dictionaries)
+                        is not Base.to_json_string([{'id': 1, 'x': 2, 'y': 3},
+                                                    {'id': 2, 'x': 4, 'y': 5}]))
+
+    def test_save_to_file(self):
+        """Test for save_to_file"""
+        list_dictionaries = [{'id': 1, 'x': 2, 'y': 3},
+                             {'id': 2, 'x': 4, 'y': 5}]
+        self.assertTrue(Base.to_json_string(list_dictionaries) is not
+                        Base.save_to_file(None))
+        self.assertTrue(Base.to_json_string(list_dictionaries) is not
+                        Base.save_to_file([]))
 
 
 if __name__ == '__main__':
