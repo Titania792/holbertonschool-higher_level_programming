2f6e9170d6c94e2fa066174605e41a06bb76bb0c
Titania792 <darkwarrior792@gmail.com>
2022-08-01 12:53:59 -0700

    changing task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,8 +5,8 @@ function secondBiggest (list) { // list is the array to be sorted
     return 0;
   }
   list.sort(function (a, b) { // sort the array in ascending order
-      return a - b;
-        });
-    return list[list.length - 2]; // return the second biggest element
+    return a - b;
+  });
+  return list[list.length - 2]; // return the second biggest element
 }
 console.log(secondBiggest(process.argv.slice(2))); // print the second biggest element of the array
