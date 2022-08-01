199866f4e90969140ed5ab802a76d528eb5950c7
Titania792 <darkwarrior792@gmail.com>
2022-08-01 10:22:43 -0700

    task 5 correct

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,8 +2,7 @@
 const args = process.argv;
 
 if (args.length === 3 && parseInt(args[2])) {
-    console.log('My number:', parseInt(args[2]));
-}
-else {
-    console.log('Not a number');
+  console.log('My number:', parseInt(args[2]));
+} else {
+  console.log('Not a number');
 }
