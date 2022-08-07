4e5f7335fb9f7f0b74f967f3eb623b5a9b176d19
Titania792 <darkwarrior792@gmail.com>
2022-08-07 14:56:59 -0700

    pycode

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     import sys
 
     con = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+                          passwd=sys.argv[2], db=sys.argv[3])
     cursor = con.cursor()
     cursor.execute("SELECT cities.id, cities.name, states.name FROM cities\
                 JOIN states ON cities.state_id = states.id\
