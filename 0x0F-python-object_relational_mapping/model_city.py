1da6943bff5f26d18c7e3814243c49cf47dc1130
Titania792 <darkwarrior792@gmail.com>
2022-08-07 19:05:05 -0700

    fixing 14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""  """
+""" Write a Python file similar to model_state.py named
+model_city.py that contains the class definition of a City """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
