846481a67df4f2f10cfa4d4098043a1af23e236f
Titania792 <darkwarrior792@gmail.com>
2022-08-07 19:04:00 -0700

    fix 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
                             argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(engine)
-    for state in session.query(State).filter(State.name.contains('a')):
+    for state in session.query(State).order_by(State.id)\
+            .filter(State.name.contains('a')):
         session.delete(state)
     session.commit()
     session.close()
