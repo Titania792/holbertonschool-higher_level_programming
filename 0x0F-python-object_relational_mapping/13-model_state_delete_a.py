4ff1b3531eca1511c2a4994ddce7f0251687c9e9
Titania792 <darkwarrior792@gmail.com>
2022-08-07 19:00:19 -0700

    last mandatory

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+""" Write a script that deletes all State objects with a name
+containing the letter a from the database hbtn_0e_6_usa """
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                            argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    for state in session.query(State).filter(State.name.contains('a')):
+        session.delete(state)
+    session.commit()
+    session.close()
--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+""" Write a Python file similar to model_state.py named
+model_city.py that contains the class definition of a City """
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from model_city import City
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                            argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    for state in session.query(State).order_by(State.id):
+        for city in session.query(City).filter(
+                City.state_id == state.id).order_by(City.id):
+            print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.close()
