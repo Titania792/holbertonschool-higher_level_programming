9b6524eaece3aed866013f6123b1d7fb38dea25b
Titania792 <darkwarrior792@gmail.com>
2022-08-19 16:13:49 -0300

    fix task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,10 +4,10 @@ displays the body of the response """
 if __name__ == "__main__":
     import requests
     import sys
-    with requests.get(sys.argv[1]) as response:  # open url and read response
-        if response.status_code >= 400:  # if response is not ok
-            # .status_code Returns a number that indicates the status
-            print("Error code: {}".format(response.status_code))
-        else:
-            # .text returns the content of the response, in unicode
-            print(response.text)
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:  # if response is not ok
+        # .status_code Returns a number that indicates the status
+        print("Error code: {}".format(r.status_code))
+    else:
+        # .text returns the content of the response, in unicode
+        print(r.text)
