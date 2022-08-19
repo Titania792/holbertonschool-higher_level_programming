d49d782b39dcbfcb5a28ccc614a3c63c5ccf295a
Titania792 <darkwarrior792@gmail.com>
2022-08-19 15:49:32 -0300

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 """ Script that takes in a URL, sends a request to the URL and
-displays the body of the response 
-You have to manage urllib.error.HTTPError exceptions and print: Error code: followed by the HTTP status code
-You must use the packages urllib and sys
-You are not allowed to import other packages than urllib and sys
-You don't need to check arguments passed to the script (number or type)
-You must use the with statement"""
+displays the body of the response """
 if __name__ == "__main__":
     from urllib import request, error
     import sys
