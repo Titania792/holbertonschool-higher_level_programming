426a893664e3ba6dcf12430bc0e787119fd63291
Titania792 <darkwarrior792@gmail.com>
2022-08-19 14:33:05 -0300

    fixing task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ Python script that fetches https://intranet.hbtn.io/status """
+
+
 if __name__ == "__main__":
-    import urllib.request
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    from urllib import request
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
