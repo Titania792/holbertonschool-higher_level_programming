59697672b295dedad889a4e610e9488f259438fe
Titania792 <darkwarrior792@gmail.com>
2022-08-19 15:48:30 -0300

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,12 +2,10 @@
 """ Script that takes in a URL, sends a request to the URL and
 displays the body of the response """
 if __name__ == "__main__":
-    import requests
+    from urllib import request, error
     import sys
-    with requests.get(sys.argv[1]) as response:  # open url and read response
-        if response.status_code >= 400:  # if response is not ok
-            # .status_code Returns a number that indicates the status
-            print("Error code: {}".format(response.status_code))
-        else:
-            # .text returns the content of the response, in unicode
-            print(response.text)
+    try:
+        with request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as err:
+        print('Error code: {}'.format(err.code))
