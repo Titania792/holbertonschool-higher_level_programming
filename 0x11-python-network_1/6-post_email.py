cb7b7c49ea27f46eed3f34c6e9b9b05fda4c6758
Titania792 <darkwarrior792@gmail.com>
2022-08-19 16:48:06 -0300

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Script that takes in a URL and an email, sends a POST request to the passed
+URL with the email as a parameter, and displays the body of the response """
+if __name__ == "__main__":
+    import requests
+    import sys
+    email = {'email': sys.argv[2]}
+    r = requests.post(sys.argv[1], email)
+    print(r.text)
