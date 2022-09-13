1eb0176363bfc24c2ed8ccb8f95c251571b1c603
Tatiana Fernández <darkwarrior792@gmail.com>
2022-09-12 23:32:26 -0300

    fh going back

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 
-const axios = 'https://swapi-api.hbtn.io/api/films/' + id;
+const axios = require('axios').default;
 const id = process.argv.slice(2)[0];
-const request = require('request');
-request(axios, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(JSON.parse(body).title);
-  }
-});
+axios.get('https://swapi-api.hbtn.io/api/films/' + id)
+  .then(function (response) {
+    console.log(response.data.title);
+  });
